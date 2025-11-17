de1670502d2b15d066848b6c6e5bf40eeeae8bcb
dppatel8085 <devrajpatel8085@gmail.com>
2025-11-17 19:14:16 +0530

    api implement

--- src/Route/userRoute.js
+++ src/Route/userRoute.js
@@ -1,7 +1,84 @@
 import { Router } from "express";
+import { getAboutEla, getAboutIsee, getBanner, getBlog, getChapter, getCompetition, getCoreEla, getElaDetail, getFooterBanner, getKangaroo, getKangarooDetail, getLanguage, getMathTest, getOffer, getPlan, getPricing, getRegistration, getScience, getScienceDetail, getStory, getTrust, getTutoring, getWhyChoose } from "../Controller/userController.js";
 
 const userRouter = Router();
 
+/*------------------- banner-------- */
+userRouter.get("/banner", getBanner)
 
+/*------ why choose us--------*/
+userRouter.get("/why-choose", getWhyChoose)
+
+/*----------------------offers----------------------*/
+userRouter.get("/offers", getOffer)
+
+/*------------------------story ------------------*/
+userRouter.get("/story", getStory)
+
+
+/*------------------trust & credability ----------------------*/
+userRouter.get("/trust", getTrust)
+
+
+/*----------------------------pricing Or Plan --------------------------*/
+userRouter.get("/plan", getPlan)
+
+
+/*---------------------footer banner-----------------*/
+userRouter.get("/footer-banner", getFooterBanner)
+
+
+/*============================ pricing ===============================*/
+userRouter.get("/pricing", getPricing)
+
+/*============================math section============================*/
+
+
+/*---------------------math test -----------------------*/
+userRouter.get("/math-test", getMathTest)
+
+/*---------------------tutoring  -----------------------*/
+userRouter.get("/tutoring", getTutoring)
+
+/*---------------------chapter  -----------------------*/
+userRouter.get("/chapter", getChapter)
+
+
+/*---------------------competition  -----------------------*/
+userRouter.get("/competition", getCompetition)
+
+/*---------------------math kangaroo test prep   -----------------------*/
+userRouter.get("/kangaroo-test", getKangaroo)
+
+/*---------------------math kangaroo test detail   -----------------------*/
+userRouter.get("/detail-kangaroo", getKangarooDetail)
+
+/*---------------------about common core science -----------------------*/
+userRouter.get("/about-science", getScience)
+
+/*---------------------about common core  detail   -----------------------*/
+userRouter.get("/science-detail", getScienceDetail)
+
+/*======================blog maa amc  ===========================*/
+userRouter.get("/blog", getBlog)
+
+/*=============english================*/
+/*---------------------language and arts -----------------------*/
+userRouter.get("/language", getLanguage)
+
+/*---------------------about common core ela -----------------------*/
+userRouter.get("/about-core-ela", getCoreEla)
+
+/*---------------------about common core  detail   -----------------------*/
+userRouter.get("/core-ela-detail", getElaDetail)
+
+/*--------------------all you need to know about ela---------------*/
+userRouter.get("/about-ela", getAboutEla)
+
+/*--------------------all about isee test---------------*/
+userRouter.get("/about-isee-test", getAboutIsee)
+
+/*--------------------registration details---------------*/
+userRouter.get("/registration", getRegistration)
 
 export default userRouter;
